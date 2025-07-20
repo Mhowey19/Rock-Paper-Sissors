8b0d153db75b01dffc42b66c2bf3e155421cdffd
Mhowey19 <michaelhowey5@gmail.com>
2025-07-19 23:31:32 -0400

    added user input and made it fully functional

--- Rock-Paper-Sissors/java.js
+++ Rock-Paper-Sissors/java.js
@@ -6,23 +6,29 @@ const guess = document.getElementById('compGuess')
 let userInput = document.getElementById('user-input')
 let randomNumberGen
 let userResult
+let userFinalAnswer
 //Math.random allows for a random number to generate
 // I gave each input a value | rock = 1 , sissor = 2 , paper = 3
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => { //Converts user input into a value that can be checked within the if statments
     e.preventDefault()
+    console.log(userInput.value)
+    if (userInput.value == 'rock' || userInput.value == 'Rock') {
+        rockChoice()
 
-    if (userInput.value == 'rock') {
-        const userResult = 1
-        console.log(userResult)
+    }
+    else if (userInput.value == "sissors" || userInput.value == "Sissors") {
+        sissorChoice()
+
+    }
+    else if (userInput.value == "paper" || userInput.value == "Paper") {
+        paperChoice()
     }
     else {
-        console.log('burber')
+        console.log("input valid submission")
     }
 
 })
-
-console.log(Number(Number(rockButton.value)))
 
 
 guess.addEventListener('click', (e) => {
@@ -31,10 +37,33 @@ guess.addEventListener('click', (e) => {
     e.preventDefault()
 })
 
+
 rockButton.addEventListener('click', (e) => {
     e.preventDefault()
     randomNumberGen = randomNumberGen = Math.floor(Math.random() * 3) + 1;
     console.log(randomNumberGen)
+    rockChoice()
+
+});
+
+sissorButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    randomNumberGen = randomNumberGen = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumberGen)
+    sissorChoice()
+
+});
+
+paperButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    randomNumberGen = randomNumberGen = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumberGen)
+    paperChoice()
+
+});
+
+function rockChoice() {
+    randomNumberGen = randomNumberGen = Math.floor(Math.random() * 3) + 1;
 
     if (randomNumberGen + Number(rockButton.value) == 2) { //Rock vs Rock 
         console.log("Tie")
@@ -45,39 +74,31 @@ rockButton.addEventListener('click', (e) => {
     else if (randomNumberGen + Number(rockButton.value) == 4) ( //Rock vs Paper
         console.log('You Lose')
     )
-});
-
-
-sissorButton.addEventListener('click', (e) => {
-    e.preventDefault()
+}
+function sissorChoice() {
     randomNumberGen = randomNumberGen = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNumberGen)
 
-    if (randomNumberGen + Number(sissorButton.value) == 3) { // Sissors vs Rock 
+    if (randomNumberGen + 2 == 3) { // Sissors vs Rock 
         console.log("You Lose")
     }
-    else if (randomNumberGen + Number(sissorButton.value) == 4) { // Sissors vs Sissors
+    else if (randomNumberGen + 2 == 4) { // Sissors vs Sissors
         console.log('Tie')
     }
-    else if (randomNumberGen + Number(sissorButton.value) == 5) ( // Sissors vs Paper
+    else if (randomNumberGen + 2 == 5) ( // Sissors vs Paper
         console.log('You Win')
-
     )
-});
+}
 
-paperButton.addEventListener('click', (e) => {
-    e.preventDefault()
+function paperChoice() {
     randomNumberGen = randomNumberGen = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNumberGen)
 
-    if (randomNumberGen + Number(paperButton.value) == 4) { // Paper vs Rock 
+    if (randomNumberGen + 3 == 4) { // Paper vs Rock 
         console.log("You Win")
     }
-    else if (randomNumberGen + Number(paperButton.value) == 5) { // Paper vs Sissors
+    else if (randomNumberGen + 3 == 5) { // Paper vs Sissors
         console.log('You Lose')
     }
-    else if (randomNumberGen + Number(paperButton.value) == 6) (// Paper vs Paper
+    else if (randomNumberGen + 3 == 6) (// Paper vs Paper
         console.log('Tie')
-
     )
-});
+}
